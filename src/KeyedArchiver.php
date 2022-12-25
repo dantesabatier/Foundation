20f08a8c04d8398afc6d0f79484d0164c229a7ca
Dante <dantesabatier@me.com>
2022-12-25 04:33:00 -0600

    Update KeyedArchiver.php

--- src/KeyedArchiver.php
+++ src/KeyedArchiver.php
@@ -15,6 +15,6 @@ class KeyedArchiver
     public static function archivedData(/** @noinspection PhpUnusedParameterInspection */ mixed $object, bool $requiresSecureCoding = true): string
     {
         $decoded = base64_decode($object, true);
-        return unsafe_value(fn(): string => !$decoded || ($decoded && !is_serialized($decoded)) ? base64_encode(serialize($object)) : $object);
+        return unsafe_value(fn(): string => !$decoded || !is_serialized($decoded) ? base64_encode(serialize($object)) : $object);
     }
 }
