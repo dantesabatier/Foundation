b5cadf0c8922b0d0a1c2a20ff2ca4f3149a02a6b
Dante <dantesabatier@me.com>
2023-01-03 12:01:39 -0600

    Update CollectionAlgorithms.php

--- src/CollectionAlgorithms.php
+++ src/CollectionAlgorithms.php
@@ -3,6 +3,8 @@
 namespace Sabatier\Foundation;
 
 use Closure;
+use InvalidArgumentException;
+use OutOfBoundsException;
 use Sabatier\Foundation\Predicates\Predicate;
 use Sabatier\Foundation\Predicates\PredicateUtilities;
 
@@ -20,8 +22,12 @@ trait CollectionAlgorithms
 
     public function offsetGet(mixed $offset): mixed
     {
-        assert(is_int($offset), sprintf("Invalid argument: expecting int, \"%s\" given", typeof($offset)));
-        assert($this->offsetExists($offset), sprintf("%s %s(%s) index \"%s\" out of bounds [%s...<%s]", $this->debugDescription(), __FUNCTION__, $offset, $offset, $this->startIndex(), $this->endIndex()));
+        if (!is_int($offset)) {
+            throw new InvalidArgumentException(sprintf("Invalid argument: expecting int, \"%s\" given", typeof($offset)));
+        }
+        if (!$this->offsetExists($offset)) {
+            throw new OutOfBoundsException(sprintf("%s %s(%s) index \"%s\" out of bounds [%s...<%s]", $this->debugDescription(), __FUNCTION__, $offset, $offset, $this->startIndex(), $this->endIndex()));
+        }
         return $this->reserved[$offset];
     }
 
