6f022024137fd261b6f6597cd01ce9a8e8ddc2dd
Dante <dantesabatier@me.com>
2023-01-14 13:21:29 -0600

    Update RangeReplaceableCollectionAlgorithms.php

--- src/RangeReplaceableCollectionAlgorithms.php
+++ src/RangeReplaceableCollectionAlgorithms.php
@@ -9,6 +9,12 @@ trait RangeReplaceableCollectionAlgorithms
 {
     use MutableCollectionAlgorithms;
 
+    public static function repeating(mixed $value, int $count): self
+    {
+        assert($count >= 0, "Invalid argument: count must be zero or greater");
+        return new self(array_fill(0, $count, $value));
+    }
+
     public function replaceSubrange(Range $subrange, Collection $newElements): void
     {
         assert($subrange->count() <= $this->count() && $subrange->count() === $newElements->count(), "Invalid argument: the range's upper bound must be less or equal to the count of the receiver and, range and collection must have the same number of elements");
@@ -21,11 +27,5 @@ trait RangeReplaceableCollectionAlgorithms
     public function removeSubrange(Range $subrange): void
     {
         $this->removeAll(fn(mixed $e, int $i): bool => $subrange->contains($i));
-    }
-
-    public static function repeating(mixed $value, int $count): self
-    {
-        assert($count >= 0, "Invalid argument: count must be zero or greater");
-        return new self(array_fill(0, $count, $value));
     }
 }
