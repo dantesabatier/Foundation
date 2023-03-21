10bb8337f08af897f6c8bf7e35c73142c71b0a83
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-03-21 09:00:48 -0600

    Update WebSocketFrame.php

--- src/Networking/WebSocketFrame.php
+++ src/Networking/WebSocketFrame.php
@@ -2,6 +2,7 @@
 
 namespace Sabatier\Foundation\Networking;
 
+/** @internal */
 readonly class WebSocketFrame
 {
     public function __construct(public string $payload, public URLSessionWebSocketOperationCode $operationCode = URLSessionWebSocketOperationCode::cont, public bool $isFinal = false, public bool $isMasked = true)
