bff0a5ba78a805481f340c74b7f41ba81152e49d
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-03-23 14:42:46 -0600

    Update URLSessionWebSocketTask.php

--- src/Networking/URLSessionWebSocketTask.php
+++ src/Networking/URLSessionWebSocketTask.php
@@ -154,7 +154,7 @@ class URLSessionWebSocketTask extends URLSessionTask
     public function noteReceivedPong(): void
     {
         if (!($completionHandler = $this->pongCompletionHandlers->popFirst())) {
-            $this->close(URLSessionWebSocketTaskCloseCode::protocolError);
+            //$this->close(URLSessionWebSocketTaskCloseCode::protocolError);
             return;
         }
         $completionHandler(null);
