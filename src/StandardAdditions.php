0f15df2e5aaf9ee93184cd61044372d0bdd4b703
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-03-31 19:03:09 -0600

    Update StandardAdditions.php

--- src/StandardAdditions.php
+++ src/StandardAdditions.php
@@ -375,7 +375,7 @@ function is_hidden(string $filename): bool
 {
     /** @phpstan-ignore-next-line */
     if (USE_UNSAFE_FUNCTIONS && TARGET_OS_WINDOWS) {
-        $attributes = trim(unsafe_value(fn(): string|bool|null => shell_exec("FOR %A IN (" . "\"" . $filename . "\"" . ") DO @ECHO %~aA")));
+        $attributes = trim(unsafe_value(fn(): string => (string)shell_exec("FOR %A IN (" . "\"" . $filename . "\"" . ") DO @ECHO %~aA")));
         return $attributes[3] === "h" || $attributes[4] === "s";
     }
     return str_starts_with($filename, ".");
