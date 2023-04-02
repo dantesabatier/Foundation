1ab173fcc0e29ea8d620a1a04dc4ae6a5531187e
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-04-02 06:55:03 -0500

    Update HTTPURLProtocol.php

--- src/Networking/HTTPURLProtocol.php
+++ src/Networking/HTTPURLProtocol.php
@@ -14,7 +14,6 @@ use Sabatier\Foundation\ProcessInfo;
 use Sabatier\Foundation\URL;
 use Sabatier\Foundation\URLFileTypeMappings;
 use function Sabatier\Foundation\fatal_error;
-use function Sabatier\Foundation\in_range;
 use function Sabatier\Foundation\string_is_equal;
 use const Sabatier\Foundation\CocoaErrorDomain;
 use const Sabatier\Foundation\LocalizedDescriptionKey;
@@ -343,8 +342,16 @@ class HTTPURLProtocol extends NativeProtocol
         $behaviour = $session->behaviour($this->task);
         switch ($behaviour->rawValue) {
             case TaskBehaviourRawValue::taskDelegate:
-                if (!in_range($response->statusCode, HTTPStatusCode::movedPermanently, HTTPStatusCode::permanentRedirect)) {
-                    $this->client?->urlProtocolDidReceiveCacheStoragePolicy($this, $response, URLCacheStoragePolicy::notAllowed);
+                switch ($response->statusCode) {
+                    case HTTPStatusCode::movedPermanently:
+                    case HTTPStatusCode::found:
+                    case HTTPStatusCode::seeOther:
+                    case HTTPStatusCode::temporaryRedirect:
+                    case HTTPStatusCode::permanentRedirect:
+                        break;
+                    default:
+                        $this->client?->urlProtocolDidReceiveCacheStoragePolicy($this, $response, URLCacheStoragePolicy::notAllowed);
+                        break;
                 }
                 break;
             case TaskBehaviourRawValue::noDelegate:
