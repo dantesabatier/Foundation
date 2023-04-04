2ac3076ee3b3fc83b4e9e10bf4dc5c3d1f7a1adc
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-04-04 05:44:28 -0500

    Update TaskBody.php

--- src/Networking/TaskBody.php
+++ src/Networking/TaskBody.php
@@ -3,9 +3,16 @@
 namespace Sabatier\Foundation\Networking;
 
 use Exception;
+use Sabatier\Foundation\Error;
 use Sabatier\Foundation\FileAttributeKey;
 use Sabatier\Foundation\FileManager;
 use Sabatier\Foundation\URL;
+use const Sabatier\Foundation\CocoaErrorDomain;
+use const Sabatier\Foundation\FileNoSuchFileError;
+use const Sabatier\Foundation\FileReadNoPermissionError;
+use const Sabatier\Foundation\URLErrorFileDoesNotExist;
+use const Sabatier\Foundation\URLErrorNoPermissionsToReadFile;
+use const Sabatier\Foundation\URLErrorUnknown;
 
 /** @internal */
 readonly class TaskBody
@@ -43,6 +50,18 @@ readonly class TaskBody
             TaskBodyRawValue::none, TaskBodyRawValue::stream => 0,
             TaskBodyRawValue::data => strlen((string)$this->data),
             TaskBodyRawValue::file => FileManager::default()->attributesOfItem((string)$this->fileURL?->path)[FileAttributeKey::size]
+        };
+    }
+
+    public function errorCode(Error $error): int
+    {
+        return match ($error->domain) {
+            CocoaErrorDomain => match ($error->code) {
+                FileNoSuchFileError => URLErrorFileDoesNotExist,
+                FileReadNoPermissionError => URLErrorNoPermissionsToReadFile,
+                default => URLErrorUnknown
+            },
+            default => URLErrorUnknown
         };
     }
 }
