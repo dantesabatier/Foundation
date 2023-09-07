e7dd53a505e5dff60958f0611060e5a2ef9adb6a
Dante <dante.palacios@holatrabajo.com>
2023-09-07 09:36:36 -0600

    Update ArrayConverter.php

--- src/ArrayConverter.php
+++ src/ArrayConverter.php
@@ -3,18 +3,15 @@
 namespace Sabatier\Foundation;
 
 /**
- * @template T
  * @internal
  */
 readonly class ArrayConverter
 {
-    /** @var ArrayClass<T> */
     public ArrayClass $array;
-    /** @var Dictionary<T> */
     public Dictionary $dictionary;
 
     /**
-     * @param array<array-key, T> $reserved
+     * @param array<array-key, mixed> $reserved
      */
     public function __construct(private array $reserved)
     {
@@ -24,66 +21,29 @@ readonly class ArrayConverter
 
     public function __get(string $name)
     {
-        $this->$name = match ($name) {
-            "array" => $this->array(),
-            "dictionary" => $this->dictionary(),
+        return $this->$name = match ($name) {
+            "array" => $this->newCollection($this->reserved, ArrayClass::class),
+            "dictionary" => $this->newCollection(is_sequential($this->reserved) ? array_combine(array_map(fn(int $i): string => human_readable_value($i), array_keys($this->reserved)), array_values($this->reserved)) : $this->reserved, Dictionary::class),
             default => throw new UndefinedKeyException()
         };
     }
 
     /**
-     * @param array<T> $array
-     * @return ArrayClass<T>
+     * @param array $array
+     * @param class-string<ArrayClass|Dictionary> $class
+     * @return mixed
      */
-    private function newArray(array $array): ArrayClass
+    private function newCollection(array $array, string $class): mixed
     {
-        /** @var ArrayClass<mixed> $arrayClass */
-        $arrayClass = new ArrayClass();
-        foreach ($array as $element) {
-            if (is_array($element)) {
-                $arrayClass[] = is_sequential($element) ? $this->newArray($element) : $this->newDictionary($element);
+        /** @psalm-suppress UnsafeInstantiation */
+        $collection = new $class();
+        foreach ($array as $i => $e) {
+            if (is_array($e)) {
+                $collection[$i] = $this->newCollection($e, is_sequential($e) ? ArrayClass::class : Dictionary::class);
             } else {
-                $arrayClass[] = $element ?? Nil::nil();
+                $collection[$i] = $e ?? Nil::nil();
             }
         }
-        return $arrayClass;
-    }
-
-    /**
-     * @param array<T> $array
-     * @return Dictionary<T>
-     */
-    private function newDictionary(array $array): Dictionary
-    {
-        /** @var Dictionary<mixed> $dictionary */
-        $dictionary = new Dictionary();
-        foreach ($array as $key => $value) {
-            if (is_array($value)) {
-                $dictionary[$key] = is_sequential($value) ? $this->newArray($value) : $this->newDictionary($value);
-            } else {
-                $dictionary[$key] = $value ?? Nil::nil();
-            }
-        }
-        return $dictionary;
-    }
-
-    /**
-     * @return ArrayClass<T>
-     */
-    private function array(): ArrayClass
-    {
-        return $this->newArray($this->reserved);
-    }
-
-    /**
-     * @return Dictionary<T>
-     */
-
-    private function dictionary(): Dictionary
-    {
-        if (!is_sequential($this->reserved)) {
-            return $this->newDictionary($this->reserved);
-        }
-        return $this->newDictionary(array_combine(array_map(fn(int $i): string => human_readable_value($i), array_keys($this->reserved)), array_values($this->reserved)));
+        return $collection;
     }
 }
