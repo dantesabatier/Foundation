afc0a21a7775e9269cef12fa75816829078520be
Dante <dante.palacios@holatrabajo.com>
2023-11-02 12:59:31 -0600

    Update ConstantsInternal.php

--- src/ConstantsInternal.php
+++ src/ConstantsInternal.php
@@ -4,10 +4,6 @@ namespace Sabatier\Foundation;
 
 /** @internal */
 const USE_UNSAFE_FUNCTIONS = false;
-/** @internal */
-const LOAD_MAIN_BUNDLE_USING_DOCUMENT_ROOT_DIRECTORY_URL = true;
-/** @var float */
-const FlOAT_EPSILON = 1.0e-5;
 /** @var string */
 const APACHE_MIME_TYPES_URL = "https://svn.apache.org/repos/asf/httpd/httpd/trunk/docs/conf/mime.types";
 /** @var int */
