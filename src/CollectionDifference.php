3b2b94f1401a84a1a2732d94ec2ab5ffeb9febd0
Dante <dantesabatier@me.com>
2023-11-11 08:44:47 -0600

    Update CollectionDifference.php

--- src/CollectionDifference.php
+++ src/CollectionDifference.php
@@ -4,6 +4,7 @@ namespace Sabatier\Foundation;
 
 /**
  * A collection of insertions and removals that describe the difference between two ordered collection states.
+ * @template ChangeElement
  */
 class CollectionDifference extends ObjectClass
 {
