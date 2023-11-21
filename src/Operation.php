77606c4e78bc6b3b1e004b7ebf126e5300a1d363
Dante <dante.palacios@holatrabajo.com>
2023-11-21 13:21:09 -0600

    Update Operation.php

--- src/Operation.php
+++ src/Operation.php
@@ -11,25 +11,24 @@ namespace Sabatier\Foundation;
 
 use Closure;
 use Fiber;
-use Throwable;
 
 /**
  * An abstract class that represents the code and data associated with a single task.
+ * @property-read bool $isCancelled A Boolean value indicating whether the operation has been cancelled.
+ * @property-read bool $isExecuting A Boolean value indicating whether the operation is currently executing.
+ * @property-read bool $isFinished A Boolean value indicating whether the operation has finished executing its task.
+ * @property-read bool $isConcurrent A Boolean value indicating whether the operation executes its task asynchronously. Use the {@see isAsynchronous} property instead. The value of this property is true for operations that run asynchronously with respect to the current thread or false for operations that run synchronously on the current thread. The default value of this property is false.
+ * @property-read bool $isAsynchronous A Boolean value indicating whether the operation executes its task asynchronously. The value of this property is true for operations that run asynchronously with respect to the current thread or false for operations that run synchronously on the current thread. The default value of this property is false.
+ * @property-read bool $isReady A Boolean value indicating whether the operation can be performed now. The readiness of operations is determined by their dependencies on other operations and potentially by custom conditions that you define. The Operation class manages dependencies on other operations and reports the readiness of the receiver based on those dependencies. If you want to use custom conditions to define the readiness of your operation object, reimplement this property and return a value that accurately reflects the readiness of the receiver. If you do so, your custom implementation must get the default property value from super and incorporate that readiness value into the new value of the property. In your custom implementation, you must generate KVO notifications for the isReady key path whenever the ready state of your operation object changes.
  */
 abstract class Operation extends ObjectClass
 {
-    /** @var bool A Boolean value indicating whether the operation has been cancelled */
-    public bool $isCancelled = false;
-    /** @var bool A Boolean value indicating whether the operation is currently executing. */
-    public bool $isExecuting = false;
-    /** @var bool A Boolean value indicating whether the operation has finished executing its task. */
-    public bool $isFinished = false;
-    /** @var bool A Boolean value indicating whether the operation executes its task asynchronously. Use the {@see isAsynchronous} property instead. The value of this property is true for operations that run asynchronously with respect to the current thread or false for operations that run synchronously on the current thread. The default value of this property is false. */
-    public bool $isConcurrent = false;
-    /** @var bool A Boolean value indicating whether the operation executes its task asynchronously. The value of this property is true for operations that run asynchronously with respect to the current thread or false for operations that run synchronously on the current thread. The default value of this property is false. */
-    public bool $isAsynchronous = false;
-    /** @var bool A Boolean value indicating whether the operation can be performed now. The readiness of operations is determined by their dependencies on other operations and potentially by custom conditions that you define. The Operation class manages dependencies on other operations and reports the readiness of the receiver based on those dependencies. If you want to use custom conditions to define the readiness of your operation object, reimplement this property and return a value that accurately reflects the readiness of the receiver. If you do so, your custom implementation must get the default property value from super and incorporate that readiness value into the new value of the property. In your custom implementation, you must generate KVO notifications for the isReady key path whenever the ready state of your operation object changes. */
-    public bool $isReady = true;
+    protected bool $isCancelled = false;
+    protected bool $isExecuting = false;
+    protected bool $isFinished = false;
+    protected bool $isConcurrent = false;
+    protected bool $isAsynchronous = false;
+    protected bool $isReady = true;
     /** @var string|null The name of the operation. */
     public ?string $name = null;
     /** @var OperationQueuePriority The execution priority of the operation in an operation queue. */
@@ -48,39 +47,51 @@ abstract class Operation extends ObjectClass
         $this->dependencies = new ArrayClass();
     }
 
+    public function __get(string $name)
+    {
+        return match ($name) {
+            "isCancelled", "isExecuting", "isFinished", "isConcurrent", "isAsynchronous", "isReady" => $this->$name,
+            default => $this->valueForUndefinedKey($name)
+        };
+    }
+
+    public function __set(string $name, mixed $value): void
+    {
+        $this->willChangeValueForKey($name);
+        $this->$name = match ($name) {
+            "isExecuting", "isFinished", "isConcurrent", "isAsynchronous", "isReady" => $value,
+            "isCancelled" => (function () use ($value): bool {
+                $this->dependencies->setValueForKey($this->isCancelled, "isCancelled");
+                return $value;
+            })(),
+            default => $this->valueForUndefinedKey($name)
+        };
+        $this->didChangeValueForKey($name);
+    }
+
     /**
      * Begins the execution of the operation.
      */
     public function start(): void
     {
-        try {
-            if ($this->isCancelled || $this->isExecuting || $this->isFinished) {
-                return;
+        if ($this->isCancelled || $this->isExecuting || $this->isFinished) {
+            return;
+        }
+        $fiber = new Fiber(function (): void {
+            Fiber::suspend();
+            $this->queue->isCurrentQueue = true;
+            $this->setValueForKey(true, "isExecuting");
+            $this->main();
+            $this->setValueForKey(false, "isExecuting");
+            if ($completionBlock = $this->completionBlock) {
+                $completionBlock();
             }
-            $fiber = new Fiber(function (): void {
-                Fiber::suspend();
-                $this->queue->isCurrentQueue = true;
-                $this->willChangeValueForKey("isExecuting");
-                $this->isExecuting = true;
-                $this->didChangeValueForKey("isExecuting");
-                $this->main();
-                $this->willChangeValueForKey("isExecuting");
-                $this->isExecuting = false;
-                $this->didChangeValueForKey("isExecuting");
-                if ($completionBlock = $this->completionBlock) {
-                    $completionBlock();
-                }
-                $this->willChangeValueForKey("isFinished");
-                $this->isFinished = true;
-                $this->didChangeValueForKey("isFinished");
-                $this->queue->isCurrentQueue = false;
-            });
-            $fiber->start();
-            while (!$fiber->isTerminated()) {
-                $fiber->resume();
-            }
-        } catch (Throwable $throwable) {
-            throw new ($throwable::class)($throwable->getMessage(), $throwable->getCode());
+            $this->setValueForKey(true, "isFinished");
+            $this->queue->isCurrentQueue = false;
+        });
+        $fiber->start();
+        if (!$fiber->isTerminated()) {
+            $fiber->resume();
         }
     }
 
@@ -104,10 +115,7 @@ abstract class Operation extends ObjectClass
         if ($this->isCancelled) {
             return;
         }
-        $this->willChangeValueForKey("isCancelled");
-        $this->isCancelled = true;
-        $this->didChangeValueForKey("isCancelled");
-        $this->dependencies->setValueForKey($this->isCancelled, "isCancelled");
+        $this->setValueForKey(true, "isCancelled");
     }
 
     /**
@@ -119,9 +127,7 @@ abstract class Operation extends ObjectClass
      */
     public function addDependency(Operation $operation): void
     {
-        $this->willChangeValueForKey("isReady");
-        $this->isReady = false;
-        $this->didChangeValueForKey("isReady");
+        $this->setValueForKey(false, "isReady");
         $operation->observe("isFinished", KeyValueObservingOptions::new, function (Operation $operation, KeyValueObservedChange $change): void {
             if ($change->newValue) {
                 $this->removeDependency($operation);
@@ -139,9 +145,7 @@ abstract class Operation extends ObjectClass
     public function removeDependency(Operation $operation): void
     {
         $this->dependencies->remove($operation);
-        $this->willChangeValueForKey("isReady");
-        $this->isReady = $this->dependencies->isEmpty();
-        $this->didChangeValueForKey("isReady");
+        $this->setValueForKey($this->dependencies->isEmpty(), "isReady");
     }
 
     /**
