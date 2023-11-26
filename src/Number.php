5ff81f2e6fdfeba1ce1af513a36fd2a3311e1c3d
Dante <dantesabatier@me.com>
2023-11-26 00:25:46 -0600

    Update Number.php

--- src/Number.php
+++ src/Number.php
@@ -27,7 +27,7 @@ class Number extends Value
     public function __construct(bool|float|int|string $value)
     {
         parent::__construct($value);
-        assert(is_numeric($this->value) || is_bool($this->value), sprintf("Invalid argument, expecting a numeric value, (%s)'%s' given", typeof($this->value), human_readable_value($this->value)));
+        assert(is_numeric($this->value) || is_bool($this->value), sprintf("Invalid argument, expecting a numeric value, (%s)%s given", typeof($this->value), human_readable_value($this->value)));
         unset($this->boolValue);
         unset($this->floatValue);
         unset($this->doubleValue);
