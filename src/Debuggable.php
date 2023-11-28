996ed0f5d572db6e467accca9f160a070bf488ba
Dante <dantesabatier@me.com>
2023-11-27 22:07:34 -0600

    Update Debuggable.php

--- src/Debuggable.php
+++ src/Debuggable.php
@@ -2,23 +2,15 @@
 
 namespace Sabatier\Foundation;
 
-use JetBrains\PhpStorm\Deprecated;
-
 trait Debuggable
 {
-    #[Deprecated]
-    public static function className(): string
-    {
-        return class_name(static::class);
-    }
-
     public function description(): string
     {
-        return sprintf("<%s %s>", static::class, spl_object_id($this));
+        return sprintf("<%s %s>", class_name(static::class), spl_object_id($this));
     }
 
     public function debugDescription(): string
     {
-        return sprintf("<%s %s>", static::class, spl_object_id($this));
+        return sprintf("<%s %s>", class_name(static::class), spl_object_id($this));
     }
 }
