664bddc35baf386f79e976a49b9ec30585acfdb3
Dante <113949719+dantesabatier@users.noreply.github.com>
2024-01-07 17:42:56 -0600

    Update DataURLProtocol.php

--- src/Networking/DataURLProtocol.php
+++ src/Networking/DataURLProtocol.php
@@ -163,6 +163,7 @@ class DataURLProtocol extends URLProtocol
         if (!$decodeHeader()) {
             return null;
         }
+        /** @psalm-suppress TypeDoesNotContainType, RedundantCondition */
         if (!($data = $base64 ? $decodeBase64Body() : $decodeStringBody())) {
             return null;
         }
