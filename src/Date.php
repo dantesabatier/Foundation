ed982e44e96fab45b85f25d6ddc3175d7001ee3a
dantesabatier <dantesabatier@me.com>
2024-11-16 12:15:24 -0600

    Update Date.php

--- src/Date.php
+++ src/Date.php
@@ -28,7 +28,7 @@ class Date extends ObjectClass
         get => $this->format();
     }
     public string $debugDescription {
-        get => sprintf("<%s %s>", get_called_class(), $this->description);
+        get => sprintf("<%s %s>", get_class($this), $this->description);
     }
 
     public function __construct(?float $time = null)
