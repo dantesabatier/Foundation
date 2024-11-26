c3f3c0b708d712eb76ed9951574d3ca38b5ca09a
dantesabatier <dantesabatier@me.com>
2024-11-26 09:10:42 -0600

    Update HTTPURLResponse.php

--- src/Networking/HTTPURLResponse.php
+++ src/Networking/HTTPURLResponse.php
@@ -16,12 +16,12 @@ use function Sabatier\Foundation\string_is_equal;
  */
 class HTTPURLResponse extends URLResponse
 {
-    protected(set) string $httpVersion;
+    private(set) string $httpVersion;
     /** @var int The response's HTTP status code. */
     #[ExpectedValues(valuesFromClass: HTTPStatusCode::class)]
-    protected(set) int $statusCode;
+    private(set) int $statusCode;
     /** @var Dictionary All HTTP header fields of the response. */
-    protected(set) Dictionary $allHeaderFields;
+    private(set) Dictionary $allHeaderFields;
     public string $description {
         get => sprintf("<HTTPURLResponse %s> { URL: %s }{ status: %d, headers {\n%s} }", $this->hash, $this->url->absoluteString, $this->statusCode, $this->allHeaderFields->mapValues(fn(mixed $value, string $key): string => is_string($value) ? "\"$key\" = \"$value\";\n" : sprintf("\"%s\" = %s;\n", $key, human_readable_value($value)))->values->join(""));
     }
