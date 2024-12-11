71747e8fbbc7072e144250695c6fb4e959ff1d8e
dantesabatier <dantesabatier@me.com>
2024-12-11 10:22:16 -0600

    Update KeyValueCodingInternal.php

--- src/KeyValueCodingInternal.php
+++ src/KeyValueCodingInternal.php
@@ -41,7 +41,7 @@ function kvc_components(string $keyPath): array
     }
     $collection = "";
     $keyPathToProperty = "";
-    /** @var string[] $components */
+    /** @var array<string> $components */
     $components = preg_split(sprintf("/%s/", preg_quote(".", "/")), substring_to_index($keyPath, $idx), -1, PREG_SPLIT_NO_EMPTY);
     $numberOfComponents = count($components);
     if ($numberOfComponents) {
