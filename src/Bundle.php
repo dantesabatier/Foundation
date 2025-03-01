faef478f7aa61542e2815468760a71ac4aae429e
dantesabatier <dantesabatier@me.com>
2025-03-01 14:54:47 -0600

    Update Bundle.php

--- src/Bundle.php
+++ src/Bundle.php
@@ -250,10 +250,11 @@ final class Bundle extends ObjectClass
         }
         $languages ??= new ArrayClass([""]);
         $resources = $languages->flatMap(fn(string $language): ArrayClass => FileManager::default()->contentsOfDirectory($language ? $baseURL->appendingPathComponent($language) : $baseURL, null, DirectoryEnumerationOptions::skipsHiddenFiles))->filter(function (URL $url, int $idx, bool &$stop) use ($name, $extensions, $limit): bool {
-            $ok = $url->deletingPathExtension()->lastPathComponent === pathinfo((string)$name, PATHINFO_FILENAME) && (!$extensions instanceof ArrayClass || $extensions->containsElement($url->pathExtension));
+            $ok = $url->deletingPathExtension()->lastPathComponent === pathinfo((string)$name, PATHINFO_FILENAME) && (!$extensions instanceof ArrayClass || $extensions->isEmpty || $extensions->containsElement($url->pathExtension));
             $stop = $ok && $limit > 0 && $limit >= $idx;
             return $ok;
         });
+        error_log(sprintf("%s:%s", human_readable_value($name), human_readable_value($resources)));
         if ($resources->isEmpty) {
             return null;
         }
