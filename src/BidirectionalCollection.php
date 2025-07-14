fe792112fa82a35f52ebd9a78144344592dedb61
dantesabatier <dantesabatier@me.com>
2025-07-14 09:15:45 -0600

    Update BidirectionalCollection.php

--- src/BidirectionalCollection.php
+++ src/BidirectionalCollection.php
@@ -20,21 +20,21 @@ interface BidirectionalCollection extends Collection
     /**
      * Returns the last element of the collection that satisfies the given predicate.
      * @param Closure(Element, Index=): bool|null $where A closure that takes an element of the collection as its argument and returns a Boolean value indicating whether the element is a match.
-     * @return Element|null The last element of the collection that satisfies predicate, or nil if there is no element that satisfies predicate.
+     * @return Element|null The last element of the collection that satisfies predicate or nil if there is no element that satisfies predicate.
      */
     public function last(?Closure $where = null);
 
     /**
      * Returns the position immediately before the given index.
      * @param int $i The index value immediately before $i.
-     * @return int A valid index of the collection. $i must be greater than startIndex.
+     * @return int A valid index of the collection. The $i must be greater than startIndex.
      */
     public function indexBefore(int $i): int;
 
     /**
      * Replaces the given index with its predecessor.
      *
-     * @param int $i A valid index of the collection. $i must be greater than startIndex.
+     * @param int $i A valid index of the collection. The $i must be greater than startIndex.
      */
     public function formIndexBefore(int &$i): void;
 
@@ -48,8 +48,8 @@ interface BidirectionalCollection extends Collection
     /**
      * Returns the last index in which an element of the collection satisfies the given predicate.
      * @param Closure(Element): bool $where A closure that takes an element as its argument and returns a Boolean value that indicates whether the passed element represents a match.
-     * @return Index|null The index of the last element for which predicate returns true.
-     * If no elements in the collection satisfy the given predicate, returns nil.
+     * @return Index|null The index of the last element for which $where returns true.
+     * If no elements in the collection satisfy the given predicate returns nil.
      */
     public function lastIndex(Closure $where);
 
