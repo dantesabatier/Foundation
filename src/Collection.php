e285370dfce73a54922cae6e0e641cc17acdf236
dantesabatier <dantesabatier@me.com>
2025-10-21 09:11:31 -0600

    Update Collection.php

--- src/Collection.php
+++ src/Collection.php
@@ -49,6 +49,8 @@ interface Collection extends Sequence, ArrayAccess
 
     /**
      * Returns the first index in which an element of the collection satisfies the given predicate.
+     *
+     * Complexity O(n), where n is the length of the collection.
      * @param Closure(Element): bool $where A closure that takes an element as its argument and returns a Boolean value that indicates whether the passed element represents a match.
      * @return Index|null The index of the first element for which $where returns true.
      * If no elements in the collection satisfy the given $where returns null.
