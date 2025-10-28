2eb2f7b97cf367f6f7a5c4cfee119c600659023a
dantesabatier <dantesabatier@me.com>
2025-10-28 09:12:37 -0600

    Add docblock to UnknownKeyException class
    Introduced a docblock to describe the purpose of the UnknownKeyException, clarifying that it is thrown when an operation encounters an unknown or undefined key.

--- src/UnknownKeyException.php
+++ src/UnknownKeyException.php
@@ -4,6 +4,9 @@ namespace Sabatier\Foundation;
 
 use UnexpectedValueException;
 
+/**
+ * Represents an exception thrown when an operation encounters an unknown or undefined key.
+ */
 class UnknownKeyException extends UnexpectedValueException
 {
 }
