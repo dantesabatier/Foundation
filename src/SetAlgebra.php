cc3fabb6a79770f0852ce34e2cf8a16977ba3f3a
dantesabatier <dantesabatier@me.com>
2026-01-01 09:51:26 -0600

    Small changes

--- src/SetAlgebra.php
+++ src/SetAlgebra.php
@@ -18,17 +18,22 @@ interface SetAlgebra extends MutableCollection
 {
     /**
      * Returns a new set with the elements of both this and the given set.
-     * @param SetAlgebra<Element> $other A sequence of elements. other must be finite.
-     * @return SetAlgebra<Element> A new set with the unique elements of this set and other.
+     *
+     * Complexity: O(n + m), where n is the length of this set and m is the length of `$other`.
+     *
+     * @param SetAlgebra<Element> $other A sequence of elements. `$other` must be finite.
+     * @return SetAlgebra<Element> A new set with the unique elements of this set and `$other`.
      */
     public function union(SetAlgebra $other): SetAlgebra;
 
     /**
      * Inserts the elements of the given sequence into the set.
      *
-     * If the set already contains one or more elements that are also in other, the existing members are kept.
-     * If other contains multiple instances of equivalent elements, only the first instance is kept.
-     * @param SetAlgebra<Element> $other A sequence of elements. other must be finite.
+     * Complexity: O(n + m), where n is the length of this set and m is the length of `$other`.
+     *
+     * If the set already contains one or more elements that are also in `$other`, the existing members are kept.
+     * If `$other` contains multiple instances of equivalent elements, only the first instance is kept.
+     * @param SetAlgebra<Element> $other A sequence of elements. `$other` must be finite.
      */
     public function formUnion(SetAlgebra $other): void;
 
@@ -41,31 +46,46 @@ interface SetAlgebra extends MutableCollection
 
     /**
      * Removes the elements of the set that aren't also in the given sequence.
-     * @param SetAlgebra<Element> $other A sequence of elements. other must be finite.
+     *
+     * Complexity: O(n + m), where n is the length of this set and m is the length of `$other`.
+     *
+     * @param SetAlgebra<Element> $other A sequence of elements. `$other` must be finite.
      */
     public function formIntersection(SetAlgebra $other): void;
 
     /**
      * Returns a new set with the elements that are either in this set or in the given sequence, but not in both.
-     * @param SetAlgebra<Element> $other A sequence of elements. other must be finite.
+     *
+     * Complexity: O(n + m), where n is the length of this set and m is the length of `$other`.
+     *
+     * @param SetAlgebra<Element> $other A sequence of elements. `$other` must be finite.
      * @return SetAlgebra<Element> A new set.
      */
     public function symmetricDifference(SetAlgebra $other): SetAlgebra;
 
     /**
      * Removes the elements of the set that are also in the given sequence and adds the members of the sequence that are not already in the set.
+     *
+     * Complexity: O(n + m), where n is the length of this set and m is the length of `$other`.
+     *
      * @param SetAlgebra<Element> $other Another set.
      */
     public function formSymmetricDifference(SetAlgebra $other): void;
 
     /**
      * Removes the elements of the given set from this set.
+     *
+     * Complexity: O(n + m), where n is the length of this set and m is the length of `$other`.
+     *
      * @param SetAlgebra<Element> $other Another set.
      */
     public function subtract(SetAlgebra $other): void;
 
     /**
      * Returns a new set containing the elements of this set that do not occur in the given set.
+     *
+     * Complexity: O(n + m), where n is the length of this set and m is the length of `$other`.
+     *
      * @param SetAlgebra<Element> $other Another set.
      * @return SetAlgebra<Element> A new set.
      */
@@ -74,25 +94,32 @@ interface SetAlgebra extends MutableCollection
     /**
      * Returns a Boolean value that indicates whether the set is a subset of another set.
      *
+     * Complexity: O(n), where n is the length of this set.
+     *
      * Set A is a subset of another set B if every member of A is also a member of B.
      * @param SetAlgebra<Element> $other Another set.
-     * @return bool true if the set is a subset of other; otherwise, false.
+     * @return bool true if the set is a subset of `$other`; otherwise, false.
      */
     public function isSubset(SetAlgebra $other): bool;
 
     /**
      * Returns a Boolean value that indicates whether this set is a superset of the given set.
      *
+     * Complexity: O(n), where n is the length of `$other`.
+     *
      * Set A is a superset of another set B if every member of B is also a member of A.
      * @param SetAlgebra $other Another set.
-     * @return bool true if the set is a superset of other; otherwise, false.
+     * @return bool true if the set is a superset of `$other`; otherwise, false.
      */
     public function isSuperset(SetAlgebra $other): bool;
 
     /**
      * Returns a Boolean value that indicates whether the set has no members in common with the given set.
+     *
+     * Complexity: O(n*m), where n is the length of this set and m is the length of `$other`.
+     *
      * @param SetAlgebra<Element> $other Another set.
-     * @return bool true if the set has no elements in common with other; otherwise, false.
+     * @return bool true if the set has no elements in common with `$other`; otherwise, false.
      */
     public function isDisjoint(SetAlgebra $other): bool;
 }
