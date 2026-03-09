b856e3ffe7a656e1deae57da49abf810e034c2b3
dantesabatier <dantesabatier@me.com>
2026-03-09 14:43:56 -0600

    Add RedundantCondition to StandardAdditions.php in rector.php
    Include the RedundantCondition issue in the configuration for StandardAdditions.php to enhance code quality checks. This addition aims to improve the detection of redundant conditions during analysis, contributing to cleaner and more efficient code.

--- rector.php
+++ rector.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Psalm\Issue\RedundantCondition;
 use Rector\CodeQuality\Rector\Catch_\ThrowWithPreviousExceptionRector;
 use Rector\CodeQuality\Rector\Class_\ConvertStaticToSelfRector;
 use Rector\CodeQuality\Rector\ClassMethod\ExplicitReturnNullRector;
@@ -72,6 +73,9 @@ try {
                 __DIR__ . "/src/Progress.php"
             ],
             UseIdenticalOverEqualWithSameTypeRector::class => [
+                __DIR__ . "/src/StandardAdditions.php"
+            ],
+            RedundantCondition::class => [
                 __DIR__ . "/src/StandardAdditions.php"
             ],
             ThrowWithPreviousExceptionRector::class => [
