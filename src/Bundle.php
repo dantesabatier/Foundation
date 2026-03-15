0954f9a959828fe8eb69ae1a92da91a833d65b9c
dantesabatier <dantesabatier@me.com>
2026-03-15 17:24:27 -0600

    Refactor resource filtering logic in Bundle class for improved extension comparison
    Update the resource filtering logic in the Bundle class to enhance the comparison of file extensions. The change replaces a direct check with a case-insensitive comparison using a closure, improving clarity and ensuring more robust matching of file types.

--- src/Bundle.php
+++ src/Bundle.php
@@ -250,7 +250,7 @@ final class Bundle extends ObjectClass
         }
         $languages ??= new ArrayClass([""]);
         $resources = $languages->flatMap(fn(string $language): ArrayClass => FileManager::default()->contentsOfDirectory($language ? $baseURL->appendingPathComponent($language) : $baseURL, null, DirectoryEnumerationOptions::skipsHiddenFiles))->filter(function (URL $url, int $idx, bool &$stop) use ($name, $extensions, $limit): bool {
-            $ok = $url->deletingPathExtension()->lastPathComponent === pathinfo((string)$name, PATHINFO_FILENAME) && (!$extensions instanceof ArrayClass || $extensions->isEmpty || $extensions->containsElement($url->pathExtension));
+            $ok = $url->deletingPathExtension()->lastPathComponent === pathinfo((string)$name, PATHINFO_FILENAME) && (!$extensions instanceof ArrayClass || $extensions->isEmpty || $extensions->contains(fn(string $extension): bool => string_is_equal($url->pathExtension, $extension, CompareOptions::caseInsensitive)));
             $stop = $ok && $limit > 0 && $limit >= $idx;
             return $ok;
         });
