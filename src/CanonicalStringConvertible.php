26b4efda540ef2602126e22c986a577216a6d288
dantesabatier <dantesabatier@me.com>
2026-04-10 19:18:59 -0600

    Crear CanonicalStringConvertible.php

---
+++ src/CanonicalStringConvertible.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sabatier\Foundation;
+
+/**
+ * A deterministic, canonical string representation of an object.
+ */
+interface CanonicalStringConvertible
+{
+    public string $canonicalDescription { get; }
+}
