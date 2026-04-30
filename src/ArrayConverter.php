24579d98acf6c420dfb231dc5eabb80411e55050
dantesabatier <dantesabatier@me.com>
2026-04-30 08:04:19 -0600

    Update ArrayConverter.php

--- src/ArrayConverter.php
+++ src/ArrayConverter.php
@@ -11,7 +11,7 @@ final class ArrayConverter
         get => $this->array ??= new SequentialArrayStrategy()->convert($this->reserved);
     }
     private(set) Dictionary $dictionary {
-        get => $this->dictionary ??= new AssociativeArrayStrategy()->convert($this->reserved !== [] && is_sequential($this->reserved) ? array_combine(array_map(human_readable_value(...), array_keys($this->reserved)), array_values($this->reserved)) : $this->reserved);
+        get => $this->dictionary ??= new AssociativeArrayStrategy()->convert(is_sequential($this->reserved) ? array_combine(array_map(human_readable_value(...), array_keys($this->reserved)), array_values($this->reserved)) : $this->reserved);
     }
 
     /**
