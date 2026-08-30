3f235a4e23bf604ddc4a7379748ce6b538c82971
dantesabatier <dantesabatier@me.com>
2026-08-29 20:13:48 -0600

    Use __PROPERTY__ in URLResourceValues getters
    Replace hard-coded string keys with __PROPERTY__ in URLResourceValues property getters. This consolidates key usage to the magic __PROPERTY__ constant for all resource properties (e.g. isDirectory, fileResourceType, fileSize, creationDate, path, parentDirectory, etc.), reducing duplication and avoiding typos.

--- src/URLResourceValues.php
+++ src/URLResourceValues.php
@@ -13,63 +13,63 @@ final class URLResourceValues extends ObjectClass
 {
     /** @var bool|null True for directories. */
     public ?bool $isDirectory {
-        get => $this->allValues["isDirectory"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var string|null Returns the file system object type. */
     public ?string $fileResourceType {
-        get => $this->allValues["fileResourceType"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var int|null Total file size in bytes. */
     public ?int $fileSize {
-        get => $this->allValues["fileSize"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var bool|null True if this process (as determined by EUID) can execute a file resource or search a directory resource. */
     public ?bool $isExecutable {
-        get => $this->allValues["isExecutable"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var bool|null True for regular files. */
     public ?bool $isRegularFile {
-        get => $this->allValues["isRegularFile"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var Date|null The time the resource's attributes were last modified. */
     public ?Date $attributeModificationDate {
-        get => $this->allValues["attributeModificationDate"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var Date|null The date the resource was created. */
     public ?Date $creationDate {
-        get => $this->allValues["creationDate"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var bool|null true if the resource is a Finder alias file or a symlink, false otherwise. */
     public ?bool $isAliasFile {
-        get => $this->allValues["isAliasFile"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var bool|null True for resources normally not displayed to users. */
     public ?bool $isHidden {
-        get => $this->allValues["isHidden"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var bool|null True if this process (as determined by EUID) can read the resource. */
     public ?bool $isReadable {
-        get => $this->allValues["isReadable"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var bool|null True for symlinks. */
     public ?bool $isSymbolicLink {
-        get => $this->allValues["isSymbolicLink"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var bool|null True if this process (as determined by EUID) can write to the resource. */
     public ?bool $isWritable {
-        get => $this->allValues["isWritable"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var string|null The resource name provided by the file system. */
     public ?string $name {
-        get => $this->allValues["name"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var URL|null The resource's parent directory, if any. */
     public ?URL $parentDirectory {
-        get => $this->allValues["parentDirectory"];
+        get => $this->allValues[__PROPERTY__];
     }
     /** @var string|null The URL's path as a file system path. */
     public ?string $path {
-        get => $this->allValues["path"];
+        get => $this->allValues[__PROPERTY__];
     }
 
     /**
